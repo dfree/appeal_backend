7658c01f350eebe4d5a31e47ac7ce01de19082b0
dfree <d@dfree.co.uk>
2018-11-16 16:18:53 +0100

    Back to reality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require('socket.io')({
 io.attach(4567);
 // io change two
 // io change three
+// io change hi 5
 io.on('connection', function(socket){
 	socket.on('update', function(data){
 		socket.emit('update', data);
